48fae0e5204070a1b4e91c87c9d693e0d1939245
Murat <murathanakbyk@gmail.com>
2025-05-07 01:01:41 +0300

    remove redundant

--- src/main/java/org/desgnpatterns/leetcode/main.java
+++
@@ -1,9 +0,0 @@
-package org.desgnpatterns.leetcode;
-
-public class main {
-    public static void main(String args[]){
-        WordDictionary wordDictionary = new WordDictionary();
-        wordDictionary.addWord("a");
-        System.out.println( wordDictionary.search("."));
-    }
-}
